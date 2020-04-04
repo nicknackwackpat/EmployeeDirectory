82175db45533f495fd5aa22dcef62f5520ca62e4
nicknack <nicknicknick22@aol.com>
2020-04-03 22:02:20 -0400

    deployed to GitHub pages

---
+++ build/precache-manifest.546b1ae3ecce41a54313e5d653fc13ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99694102992f99572dd7108b430acb2d",
+    "url": "/UserDirectory/index.html"
+  },
+  {
+    "revision": "d3a5eede16f46146f451",
+    "url": "/UserDirectory/static/css/main.4492d48b.chunk.css"
+  },
+  {
+    "revision": "63117f6e26cdc413cd82",
+    "url": "/UserDirectory/static/js/2.2b6340f3.chunk.js"
+  },
+  {
+    "revision": "39ee42a9c6b75cfef987502f097de4c8",
+    "url": "/UserDirectory/static/js/2.2b6340f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a5eede16f46146f451",
+    "url": "/UserDirectory/static/js/main.f410d4b3.chunk.js"
+  },
+  {
+    "revision": "e346fe44dd8bb71e166a",
+    "url": "/UserDirectory/static/js/runtime-main.292253d1.js"
+  }
+]);
